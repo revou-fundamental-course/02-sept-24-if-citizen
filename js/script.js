7fa304637139df9c0c06642397c73f1adebea263
if-citizen <indrianafadhila@gmail.com>
2024-09-11 22:26:43 +0700

    add  javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 //javascript
-function luasSegitiga(a, t){
-    var l= 1/2*a*t;
-    return l;
+function luasSegitiga(){
+    var a = document.forms["hitung-luas-segitiga"]["pjg-alas"].value;
+    var t = document.forms["hitung-luas-segitiga"]["tinggi"].value;
+    var l = 1/2*a*t;
+    document.getElementById("detail1").innerHTML= "= 1/2 x a x t";
+    document.getElementById("detail2").innerHTML= "= 1/2 x "+ a + " x "+ t;
+    document.getElementById("detail3").innerHTML = `= ${l}`;
 }
