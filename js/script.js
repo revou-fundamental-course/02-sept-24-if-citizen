e0c74ca322fc967e0c525551b6b15f2e335ab81b
if-citizen <indrianafadhila@gmail.com>
2024-09-12 23:20:48 +0700

    jjgj done

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ function show_hide() {
     
  }
 
- function myFunction() {
+ function myDropdown() {
       document.getElementById("myDropdown").classList.toggle("show"); //adding a class show
    }
 
+// Luas Segitiga
 function luasSegitiga(){
-    // resetLuasSegitiga();
     var a = document.forms["hitung-luas-segitiga"]["pjg-alas"].value;
     var t = document.forms["hitung-luas-segitiga"]["tinggi"].value;
     //check if not null / not number
@@ -59,6 +59,52 @@ function resetKelSegitiga(){
     document.getElementById("detail-k1").innerHTML= "";
     document.getElementById("detail-k2").innerHTML= "";
     document.getElementById("detail-k3").innerHTML = "";
+}
+// =================================================================================
+// Luas Jajargenjang
+function luasJajargenjang(){
+    var a = document.forms["hitung-luas-jajargenjang"]["alas-jjgj"].value;
+    var t = document.forms["hitung-luas-jajargenjang"]["tinggi-jjgj"].value;
+    //check if not null / not number
+    if (validasi(a) && validasi(t) == true){
+        var l = 1/2*a*t;
+        document.getElementById("detail-j1").innerHTML= "= a x t";
+        document.getElementById("detail-j2").innerHTML= "= "+ a + " x "+ t;
+        document.getElementById("detail-j3").innerHTML = `= ${l}`;
+    }  else{
+        console.log("Inputan tidak sesuai");
+    }
+}
+
+function resetLuasJajargenjang(){
+    document.forms["hitung-luas-jajargenjang"]["alas-jjgj"].value = "";
+    document.forms["hitung-luas-jajargenjang"]["tinggi-jjgj"].value = "";
+    document.getElementById("detail-j1").innerHTML= "";
+    document.getElementById("detail-j2").innerHTML= "";
+    document.getElementById("detail-j3").innerHTML = "";
+}
+
+// Keliling segitiga
+function kelJajargenjang(){
+    // resetLuasSegitiga();
+    var a = document.forms["hitung-kel-jjgj"]["a"].value;
+    var b = document.forms["hitung-kel-jjgj"]["b"].value;
+    //check if not null / not number
+    if (validasi(a) && validasi(b) == true){
+        var k = 2 * (parseInt(a) + parseInt (b));
+        document.getElementById("detail-kj1").innerHTML= "= 2 x (a + b)";
+        document.getElementById("detail-kj2").innerHTML= "= 2 x ("+ a + " + "+ b + ")";
+        document.getElementById("detail-kj3").innerHTML = `= ${k}`;
+    }  else{
+        console.log("Inputan tidak sesuai");
+    }
+}
+function resetKelJajargenjang(){
+    document.forms["hitung-kel-jjgj"]["a"].value = "";
+    document.forms["hitung-kel-jjgj"]["b"].value = "";
+    document.getElementById("detail-kj1").innerHTML= "";
+    document.getElementById("detail-kj2").innerHTML= "";
+    document.getElementById("detail-kj3").innerHTML = "";
 }
 
 function validasi(variabel){
